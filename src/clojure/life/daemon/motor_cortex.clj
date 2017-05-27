59929c5195f8b0d2252bc111a53f06b8e959fa85
slightlycyborg <collinalexbell@gmail.com>
2017-05-27 12:37:19 -0400

    Motor-cortex/move translates clj data to byte-array for choreo

--- src/clojure/life/daemon/motor_cortex.clj
+++ src/clojure/life/daemon/motor_cortex.clj
@@ -1,8 +1,10 @@
 (ns life.daemon.motor-cortex
   (:import [android.bluetooth BluetoothAdapter]
-           [android.content Intent]))
+           [android.content Intent]
+           [java.nio ByteBuffer]))
 
 (def bt nil)
+(def actiity nil)
 
 (defn -throw-if-no-bt []
   (if (= bt nil)
@@ -55,37 +57,53 @@
         (do (print (.read istream))
             (recur (.available istream)))))))
 
-(def ex [
-         0x02 ;number of motors
-         0x00 ;motor id
-         0x04 ;num-commands
-         0x00 ;command id
-         0x00 0x14 ;amount0
-         0x03 0xE8 ;duration0
-         0x01 ;command id
-         0xFF 0xEC ;amount1
-         0x03 0xE8 ;duration1
-         0x02 ;command id
-         0x00 0x14 ;amount0
-         0x03 0xE8 ;duration0
-         0x03 ;command id
-         0xFF 0xEC ;amount1
-         0x03 0xE8 ;duration1
-         0x01 ;motor id
-         0x04 ;num-commands
-         0x04 ;command id
-         0x00 0x14 ;amount0
-         0x03 0xE8 ;duration0
-         0x05 ;command id
-         0xFF 0xEC ;amount1
-         0x03 0xE8 ;duration1
-         0x06 ;command id
-         0x00 0x14 ;amount0
-         0x03 0xE8 ;duration0
-         0x07 ;command id
-         0xFF 0xEC ;amount1
-         0x03 0xE8 ;duration1
-         ])
+(def ex-command
+  {0 [{:id 0 :amount 20 :duration 1000} {:id 1 :amount -20 :duration 1000}]
+   1 [{:id 2 :amount 10 :duration 1000} {:id 3 :amount -10 :duration 1000}]})
+
+
+(defn unsigned-to-signed
+  [unsigned]
+  (if (> unsigned 127)
+    (+ -128 (- unsigned 128))
+    unsigned))
+
+(defn bytes? [x]
+  (if (nil? x)
+    false
+    (= (Class/forName "[B")
+       (.getClass x))))
+
+(defn move
+  [commands]
+  (->> [
+        ;;Number of motors
+        (count commands)
+        (map
+         (fn [[key data]]
+           [
+            ;;motor id
+            key
+
+            ;;num-commands
+            (count data)
+
+            (map
+             (fn [procedure]
+               [(byte (:id procedure))
+                (-> (ByteBuffer/allocate 4)
+                    (.putShort (:amount procedure))
+                    (.putShort (:duration procedure))
+                    (.array))])
+             data)])
+         commands)]
+       (flatten)
+       (map #(if (not (bytes? %))
+               (vector (unsigned-to-signed %))
+               %))
+
+       (mapcat seq)
+       (byte-array)))
 
 
 
