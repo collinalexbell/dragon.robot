acd480b88d14cde2e40eeb5d49f00e44a3788b17
slightlycyborg <collinalexbell@gmail.com>
2017-05-29 15:31:44 -0400

    Robot moves when speaking

--- src/clojure/life/daemon/activities.clj
+++ src/clojure/life/daemon/activities.clj
@@ -1,8 +1,12 @@
 (ns life.daemon.activities
   (:require [room-cleaning-algo.core :as clean]
-            [life.daemon.brocas-area :as brocas]))
+            [life.daemon.brocas-area :as brocas]
+            [life.daemon.motor-cortex :as motor-cortex]
+            [life.daemon.list-of-motor-commands :as commands]))
 
-(clean/change-output-fn (fn [output] (brocas/speak output)))
+(clean/change-output-fn (fn [output]
+                          (motor-cortex/move commands/look-up-left)
+                          (brocas/speak output)))
 
 (defn clean-room []
   (clean/-main))
